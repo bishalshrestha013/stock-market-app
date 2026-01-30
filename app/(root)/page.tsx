9cc6f6692ac3e954a757b59312da38332cc59242
Bishal <mr.shrestha713@gmail.com>
2026-01-30 13:50:38 +0545

    feat: add separate layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,7 @@
 import { Button } from "@/components/ui/button";
 
 const Home = () => {
-  return (
-    <div>
-      <Button>Hello world</Button>
-    </div>
-  );
+  return <div className="flex min-h-screen home-wrapper">Home</div>;
 };
 
 export default Home;
